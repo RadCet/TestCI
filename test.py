0ac2b1d3eef40f260267152e4e169571ae5fcf35
RadCet <catlikemoney@gmail.com>
2022-01-24 15:39:55 +0700

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ from func import sum
 
 class TestSum(unittest.TestCase):
 	def test_list_int(self):
-		data = [1,2,3]
-		result = sum(data)
-		self.assertEqual(result, 6)
+		#data = [1,2,3]
+		result = sum(1,2)
+		self.assertEqual(result, 3)
 
 if __name__ == '__main__':
 	unittest.main()
